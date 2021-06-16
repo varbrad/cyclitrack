e8ebbbc2fe62ba2aaf82396a7f2a4386844bc116
Brad Davies <me@varbrad.dev>
2021-06-16 21:36:23 +0100

    Move to Svelte

--- src/services/date.ts
+++
@@ -1,7 +0,0 @@
-import dayjs from 'dayjs'
-import relativeTimePlugin from 'dayjs/plugin/relativeTime'
-
-dayjs.extend(relativeTimePlugin)
-
-export const now = () => dayjs()
-export const date = dayjs
